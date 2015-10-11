d98ae4c81ffd117c1285bd656958a1b1e979d787
Lucas Farah <lucas.farah@me.com>
2015-10-11 03:20:20 -0700

    tried to setup server for url parsing, but it is crashing for some reason my Redbull doesn't allow me to see.

--- GoogleMaps.py
+++ GoogleMaps.py
@@ -6,12 +6,13 @@ import datetime
 from flask import Flask, request, redirect, session, url_for
 app = Flask(__name__)
 import os
-
+from flask import current_app
 locations = []
 plusTimes = []
 steps = []
 returnDirections=[]
 def isRaining(lat,lng,time):
+	print"rain"
 	time = "40:00:00"
 	url = "http://api.weather.com/beta/datacloud/?datatype=forecast&vs=1.0&passkey=d972da81e1ce2f854f9a5560ffb6f243&type=point&var=PrecipIntensity&lat=" + str(lat) + "&lon=" + str(lng) + "&format=hourly&time=now+" + str(time)
 	u = urllib.urlopen(url)
@@ -36,6 +37,7 @@ with open('accidents.json') as data_file:
 
 def checkDanger(step):
 	#Check if it is Raining on that Step
+	pprint(step)
 	print("check")
 	stepLoc = step["start_location"]
 	lat = stepLoc["lat"]
@@ -107,32 +109,48 @@ def getTimeForStep(step):
 
 	return "00:" + ('%02d' % (plusTime/60)) + ":00"
 
-url = 'https://maps.googleapis.com/maps/api/directions/json?origin=47.806209,-122.312071&destination=47.623315048,-122.296426643&mode=driving&sensor=false&alternatives=true'
-u = urllib.urlopen(url)
-# u is a file-like object
-data = u.read()
-# print(data)
-routes = json.loads(data)
 
-steps = routes["routes"][0]["legs"][0]["steps"]
-returnDirections = list(steps)
-# pprint(steps)
-for step in steps:
-	currentTime = 0
-	if  len(plusTimes) >0:
-		currentTime = plusTimes[len(plusTimes)-1]
-
-	currentTime += int(step["duration"]["value"])
-	plusTimes.append(currentTime)
-	stepLoc = step["start_location"]
-	checkDanger(step)
-print(json.dumps(returnDirections))
 
 @app.route("/")
 def index():
-        return json.dumps(returnDirections)
+    return json.dumps(returnDirections)
+
+@app.route("/getDirections")
+def direct():
+	#current location
+	cLat = str(request.args['cLat'])
+	cLng = str(request.args['cLng'])
+	#destination's location
+	dLat = str(request.args['dLat'])
+	dLng = str(request.args['dLng'])	
+	print(cLat)
+	print(cLng)
+	print(dLat)
+	print(dLng)
+	url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + cLat + "," + cLng + "&destination=" + dLat + "," + dLng + "&mode=driving&sensor=false&alternatives=true"
+	print(url)
+	u = urllib.urlopen(url)
+	# u is a file-like object
+	data = u.read()
+	# print(data)
+	routes = json.loads(data)
+
+	steps = routes["routes"][0]["legs"][0]["steps"]
+	returnDirections = list(steps)
+	# pprint(steps)
+	for step in steps:
+		currentTime = 0
+		if  len(plusTimes) >0:
+			currentTime = plusTimes[len(plusTimes)-1]
+
+		currentTime += int(step["duration"]["value"])
+		plusTimes.append(currentTime)
+		stepLoc = step["start_location"]
+		checkDanger(step)
+	return json.dumps(returnDirections)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
