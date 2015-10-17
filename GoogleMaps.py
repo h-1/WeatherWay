1e02a061440533a0be73989a743737e260b70b0f
Lucas Farah <lucas.farah@me.com>
2015-10-17 11:38:23 -0700

    Added app properly

--- GoogleMaps.py
+++ GoogleMaps.py
@@ -252,6 +252,7 @@ def getStepsFromGoogle(cLat,cLng,dLat,dLng):
 # print(json.dumps(returnDirections))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0')
 
